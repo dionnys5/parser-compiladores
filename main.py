4f94a1114674f1e2d5d2a71bb2b39413d0fd6add
Dionnys Santos Marinho <dionnys@rede.ulbra.br>
2019-08-10 11:07:46 -0300

    Parser linguagem

--- main.py
+++ main.py
@@ -23,27 +23,72 @@ linguagem = {
 }
 
 arquivo_tokens = open('tokens.json', 'w')
-# tabela_sintaxe = open('table_sintaxe.json','w')
+tabela_sintaxe = open('table_sintaxe.json', 'w')
 
-with open('novo-text.json', 'r') as file:
+
+def token_identificador(token):
+    """
+    :param tokens: Lista de tokens
+    :return: Dicionário com a situacao do erro e o identificador do token (Se houver)
+    """
+    erro = False
+    identificador = None
+    if token in linguagem['palavras_reservadas']:
+        identificador = 'palavras_reservadas'
+    elif token in linguagem['operadores']:
+        identificador = 'operadores'
+    elif token in linguagem['terminadores']:
+        identificador = 'terminadores'
+    elif token in linguagem['identificadores']:
+        identificador = 'identificadores'
+    elif len(token) > 1 and token[0] in linguagem['numeros']:
+        try:
+            num = int(token)
+            identificador = 'sequencia'
+        except ValueError:
+            pass
+    elif token in linguagem['numeros']:
+        identificador = 'numeros'
+    else:
+        erro = True
+    return erro, identificador
+
+
+with open('novo-text.txt', 'r') as file:
     entrada = file.read()
-    tokens_saida = {}
-    tabela_sintaxe = {}
     instrucoes = entrada.split(';')
+    lista_tokens = {}
+    lista_lexica = {}
+    erro = False
     for i, instruct in enumerate(instrucoes):
         tokens = instruct.split(' ')
         if not instruct == '':
-            tokens_saida['Instrucao ' + str(i)] = {}
-            tabela_sintaxe['Instrucao ' + str(i)] = {}
+            tokens.append(';')
             for t, token in enumerate(tokens):
-                tokens_saida['Instrucao ' + str(i)][t] = token
+                tamanho = len(token)
+                identificador = token_identificador(token)
+                if identificador[0]:
+                    erro = True
+                    lista_lexica[token + ' ' + str((i, t))] = {
+                        'token': token,
+                        'erro': 'Token nao encontrado',
+                        'posicao': (i, t)
+                    }
+                    break
+                ident = identificador[1]
+                lista_lexica[token + ' ' + str((i, t))] = {
+                    'token': token,
+                    'identificacao': ident,
+                    'tamanho': tamanho,
+                    'posicao': (i, t)
+                }
+                if ident == 'sequencia' or ident == 'identificadores':
+                    lista_tokens[t] = token
+        if erro:
+            break
 
-
-
-
-
-
-    formatacao_json = json.dumps(tokens_saida)
-    arquivo_tokens.write(formatacao_json)
-    arquivo_tokens.close()
+    tokens_json = json.dumps(lista_tokens)
+    tabela_json = json.dumps(lista_lexica)
+    arquivo_tokens.write(tokens_json)
+    tabela_sintaxe.write(tabela_json)
 
